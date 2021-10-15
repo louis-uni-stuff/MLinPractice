e1c91dbd660a42e8f0d42737e46e283b70017aad
Lara McDonald <mccilara@gmail.com>
2021-10-15 14:09:03 +0200

    Change BooleanCounter to BooleanCountGetter
    better naming

--- code/feature_extraction/get_count_boolean.py
+++ code/feature_extraction/get_count_boolean.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Retrieves a count of items in list-type column or returns boolean if list is not empty.
 
 @author: lmcdonald
 """
@@ -11,7 +12,7 @@ from code.feature_extraction.feature_extractor import FeatureExtractor
 from code.util import COLUMN_RETWEET_BOOL, COLUMN_VIDEO
 
 # class for extracting the boolean value or count of a column as a feature
-class BooleanCounter(FeatureExtractor):
+class AttributeBooleanCountGetter(FeatureExtractor):
     
     # constructor
     def __init__(self, input_column, count_type = "boolean"):
@@ -21,18 +22,19 @@ class BooleanCounter(FeatureExtractor):
         self._count_type = count_type
         self._input_column = input_column
     
-    # don't need to fit, so don't overwrite _set_variables()
-    
     # compute the boolean or count based on the inputs
     def _get_values(self, inputs):
 
         result = []
 
         for i, row in enumerate(inputs[0]):
+            # for COLUMN_VIDEO or COLUMN_RETWEET_BOOL standardize binary input to 0/1 output
             if self._input_column == COLUMN_VIDEO or self._input_column == COLUMN_RETWEET_BOOL:
                 result.append(int(bool(row)))
+            # checks if list is not empty
             elif self._count_type == "boolean":
                 result.append(int(bool(ast.literal_eval(row))))
+            # returns count of items in list
             elif self._count_type == "count":
                 result.append(len(ast.literal_eval(row)))
  
