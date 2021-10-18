4e59bcb7e6ce95278586043ba3aa62f5906ba0a1
Louis Kapp <lkapp@uni-osnabrueck.de>
2021-10-18 03:03:57 +0200

    Added MLP classifier and hyperparameter optimization with mlflow

--- code/classification/grid_search.sh
+++ code/classification/grid_search.sh
@@ -3,8 +3,10 @@
 mkdir -p data/classification
 
 # specify hyperparameter values
-values_of_k=("1 2 3 4 5 6 7 8 9 10")
-
+hidden_layer_sizes=("50 100 150 200")
+activation=("identity logistic tanh relu")
+solvers=("lbfgs sgd adam")
+max_fun=("15000 22000 30000")
 
 # different execution modes
 if [ $1 = local ]
@@ -21,8 +23,16 @@ else
 fi
 
 # do the grid search
-for k in $values_of_k
-do
-    echo $k
-    $cmd 'data/classification/clf_'"$k"'.pickle' --knn $k -s 42 --accuracy --kappa
+for size in $hidden_layer_sizes; do
+for func in $activation; do
+for solver in $solvers; do
+for mf in $max_fun; do
+    echo $size
+    echo $func
+    echo $solver
+    echo $mf
+    $cmd 'data/classification/clf_'"$size"'_'"$func"'_'"$solver"'_'"$mf"'.pickle' --seed 42 --mlp $size $func $solver $mf --accuracy --mcc --informedness --balanced_accuracy --kappa --f1_score
+done
+done
+done
 done
--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -15,6 +15,8 @@ from sklearn.dummy import DummyClassifier
 from sklearn.metrics import accuracy_score, cohen_kappa_score, balanced_accuracy_score, matthews_corrcoef, f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
+from mlflow import log_metric, log_param, set_tracking_uri
+
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Classifier")
@@ -22,58 +24,87 @@ parser.add_argument("input_file", help = "path to the input pickle file")
 parser.add_argument("-s", '--seed', type = int, help = "seed for the random number generator", default = None)
 parser.add_argument("-e", "--export_file", help = "export the trained classifier to the given location", default = None)
 parser.add_argument("-i", "--import_file", help = "import a trained classifier from the given location", default = None)
+parser.add_argument("--log_dir", help = "where to log the mlflow results", default = "data/classification/mlflow")
+# Baseline Classifiers
 parser.add_argument("-m", "--majority", action = "store_true", help = "majority class classifier", default = None)
 parser.add_argument("-q", "--frequency", action = "store_true", help = "label-frequency class classifier", default = None)
+# Classifiers
 parser.add_argument("--svm", action = "store_true", help = "Support-Vector-Machine classifier", default = None)
-parser.add_argument("--mlp", action = "store_true", help = "Multi-Layered-Perceptron classifier", default = None)
+parser.add_argument("--mlp", nargs = "*", action = "append", help = "Multi-Layered-Perceptron classifier", default = None)
+parser.add_argument("--knn", action = "store_true", help = "use KNN classifier", default=None)
+# Evaluation Metrics
 parser.add_argument("-a", "--accuracy", action = "store_true", help = "evaluate using accuracy")
 parser.add_argument("-b", "--balanced_accuracy", action = "store_true", help = "evaluate using balanced accuracy")
 parser.add_argument("-n", "--informedness", action = "store_true", help = "evaluate using informedness")
 parser.add_argument("-k", "--kappa", action = "store_true", help = "evaluate using Cohen's kappa")
 parser.add_argument("-f", "--f1_score", action = "store_true", help = "evaluate using the F1 score (or F-measure)")
-parser.add_argument("--knn", action = "store_true", help = "use KNN classifier", default=None)
 parser.add_argument("--mcc", action = "store_true", help = "evaluate using Mathews Correlation coefficient")
+
 args = parser.parse_args()
+
 
 # load data
 with open(args.input_file, 'rb') as f_in:
     data = pickle.load(f_in)
 
+set_tracking_uri(args.log_dir)
+
 if args.import_file is not None:
     # import a pre-trained classifier
     with open(args.import_file, 'rb') as f_in:
-        classifier = pickle.load(f_in)
+        input_dict = pickle.load(f_in)
+
+    classifier = input_dict["classifier"]
+    for param, value in input_dict["params"].items():
+        log_param(param, value)
+
+    log_param("dataset","validation")
 
 else:   # manually set up a classifier
     
     if args.majority:
         # majority vote classifier
         print("    majority vote classifier")
+        log_param("classifier", "majority")
+        params = {"classifier": "majority"}
         classifier = DummyClassifier(strategy = "most_frequent", random_state = args.seed)
         classifier.fit(data["features"], data["labels"])
 
     elif args.frequency:
         # label-frequency classifier
         print("    label-frequency classifier")
+        log_param("classifier", "frequency")
+        params = {"classifier": "frequency"}
         classifier = DummyClassifier(strategy = "stratified", random_state = args.seed)
         classifier.fit(data["features"], data["labels"])
 
     elif args.svm:
         #  Support Vector Machine
         print("    SVM classifier")
+        log_param("classifier", "svm")
+        params = {"classifier": "svm"}
         classifier = LinearSVC(dual=False, class_weight='balanced', random_state = args.seed)
         classifier.fit(data["features"], data["labels"].ravel())
 
     elif args.knn:
         # KNN classifier
         print("    KNN classifier")
+        log_param("classifier", "knn")
+        params = {"classifier": "knn"}
         classifier = KNeighborsClassifier(algorithm="auto", weights="distance", n_neighbors=10, random_state = args.seed)
         classifier.fit(data["features"], data["labels"].ravel())
 
     elif args.mlp:
         #MLP classifier
         print("    MLP classifier")
-        classifier = MLPClassifier(random_state = args.seed, verbose = True)
+        args.mlp = [(100,), "relu", "adam", 15000] if args.mlp == [[]] else [item for sublist in args.mlp for item in sublist]
+        log_param("classifier", "mlp")
+        log_param("hidden_layer_sizes", args.mlp[0])
+        log_param("activation", args.mlp[1])
+        log_param("solver", args.mlp[2])
+        log_param("max_fun", args.mlp[3])
+        params = {"classifier": "mlp", "hidden_layer_sizes": args.mlp[0], "activation": args.mlp[1], "solver": args.mlp[2], "max_fun": args.mlp[3]}
+        classifier = MLPClassifier(hidden_layer_sizes = (int(args.mlp[0]),), activation = args.mlp[1], solver = args.mlp[2], max_fun = int(args.mlp[3]), random_state = args.seed)
         classifier.fit(data["features"], data["labels"].ravel())
 
 
@@ -89,7 +120,7 @@ if args.balanced_accuracy:
 if args.informedness:
     evaluation_metrics.append(("Informedness", lambda x,y: balanced_accuracy_score(x,y, adjusted=True)))
 if args.kappa:
-    evaluation_metrics.append(("Cohen's kappa score", cohen_kappa_score))
+    evaluation_metrics.append(("Cohens kappa score", cohen_kappa_score))
 if args.f1_score:
     evaluation_metrics.append(("F1 score", f1_score))
 if args.mcc:
@@ -99,11 +130,15 @@ if args.mcc:
 
 # compute and print them
 for metric_name, metric in evaluation_metrics:
-    print("    {0}: {1}".format(metric_name, metric(data["labels"], prediction)))
+    metric_value = metric(data["labels"], prediction)
+    log_metric(metric_name, metric_value)
+    print("    {0}: {1}".format(metric_name, metric_value))
+
     
 # export the trained classifier if the user wants us to do so
 if args.export_file is not None:
+    output_dict = {"classifier": classifier, "params": params}
     with open(args.export_file, 'wb') as f_out:
-        pickle.dump(classifier, f_out)
+        pickle.dump(output_dict, f_out)
         
         
